ec88ad93b20709453f02bcbb163fc58176e34bc5
katerinacounta <katerina.counta@gmail.com>
2020-10-13 18:58:47 +0300

    use current weather API instead of forecast

--- src/script.js
+++ src/script.js
@@ -1,5 +1,18 @@
-let apiURL = "https://api.openweathermap.org/data/2.5/forecast";
+let apiURL = "https://api.openweathermap.org/data/2.5/weather";
 let apiKEY = "2c7be81cdfd4348e785209fd2490e8d1";
+
+//SEARCH
+function searchCity(event) {
+  event.preventDefault();
+  let cityEntry = document.querySelector("#city-input").value;
+  if (cityEntry) {
+    axios
+      .get(`${apiURL}?q=${cityEntry}&units=metric&appid=${apiKEY}`)
+      .then(getCityDetails);
+  } else {
+    alert("Please enter a city");
+  }
+}
 
 //DATE
 function getFormattedDate(timestamp) {
@@ -36,55 +49,37 @@ function getCityDetails(response) {
   cityWrapper.style.visibility = "visible";
 
   let cityName = document.querySelector("#city");
-  cityName.innerHTML = response.data.city.name;
+  cityName.innerHTML = response.data.name;
 
   let cityDesc = document.querySelector("#description");
-  cityDesc.innerHTML = response.data.list[0].weather[0].description;
+  cityDesc.innerHTML = response.data.weather[0].description;
 
   let cityTempIcon = document.querySelector("#temp-icon");
   cityTempIcon.setAttribute(
     "src",
-    `https://openweathermap.org/img/wn/${response.data.list[0].weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
-  cityTempIcon.setAttribute(
-    "alt",
-    response.data.list[0].weather[0].description
-  );
+  cityTempIcon.setAttribute("alt", response.data.weather[0].description);
 
   let cityTemp = document.querySelector("#temperature");
 
-  let cityTempCelsius = Math.round(response.data.list[0].main.temp);
+  let cityTempCelsius = Math.round(response.data.main.temp);
   let cityTempFahrenheit = Math.round((`${cityTempCelsius}` * 9) / 5 + 32);
 
   cityTemp.innerHTML = `<div class="celsius-value">${cityTempCelsius}</div><div class="fahr-value hidden">${cityTempFahrenheit}</div>`;
 
   let cityHumidity = document.querySelector("#humidity");
   cityHumidity.innerHTML = `Humidity: ${Math.round(
-    response.data.list[0].main.humidity
+    response.data.main.humidity
   )}%`;
 
   let citywindSpeed = document.querySelector("#wind");
-  citywindSpeed.innerHTML = `Wind: ${Math.round(
-    response.data.list[0].wind.speed
-  )}km/h`;
+  citywindSpeed.innerHTML = `Wind: ${Math.round(response.data.wind.speed)}km/h`;
 
   let cityDate = document.querySelector("#date");
-  cityDate.innerHTML = getFormattedDate(response.data.city.timezone * 1000);
+  cityDate.innerHTML = getFormattedDate(response.data.timezone * 1000);
 
   tempConversion();
-}
-
-//SEARCH
-function searchCity(event) {
-  event.preventDefault();
-  let cityEntry = document.querySelector("#city-input").value;
-  if (cityEntry) {
-    axios
-      .get(`${apiURL}?q=${cityEntry}&units=metric&appid=${apiKEY}`)
-      .then(getCityDetails);
-  } else {
-    alert("Please enter a city");
-  }
 }
 
 let searchForm = document.querySelector("#weather-form");
